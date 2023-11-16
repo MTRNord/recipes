092b3d0d7888c477723ddf625a196161c2206f42
MTRNord <mtrnord1@gmail.com>
2023-11-16 21:01:36 +0100

    Setup github ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://recipes.nordgedanken.dev',
   integrations: [starlightLinksValidator(), starlight({
     title: 'Rezepte',
     locales: {
