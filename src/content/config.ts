e437479c221ab93edb93bdad7db2cd0e64aea14e
MTRNord <mtrnord1@gmail.com>
2023-11-17 00:19:30 +0100

    Add support for infoboxes and descriptions

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,17 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+	docs: defineCollection({
+		schema: (context) => {
+			return docsSchema()(context).extend({
+				info: z.optional(z.array(z.object({
+					title: z.optional(z.string()),
+					text: z.string()
+				}))),
+				description: z.optional(z.string())
+			})
+		}
+	}),
 	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
