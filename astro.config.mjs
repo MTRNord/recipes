c1a7d5054187be233434a44685afacb6b86f2d2e
MTRNord <mtrnord1@gmail.com>
2024-03-01 13:30:59 +0100

    Update astro and starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import markdoc from "@astrojs/markdoc";
 import starlightLinksValidator from 'starlight-links-validator';
@@ -9,43 +9,51 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   prefetch: true,
   site: 'https://recipes.nordgedanken.dev',
-  integrations: [starlightLinksValidator(), tailwind({
-    // Disable the default base styles:
-    applyBaseStyles: false,
-  }), starlight({
-    title: 'Rezepte',
-    defaultLocale: 'de-de',
-    locales: {
-      'de-de': {
-        label: 'Deutsch',
-        lang: 'de-DE'
+  image: {
+    service: passthroughImageService(),
+  },
+  integrations: [
+    starlightLinksValidator(),
+    tailwind({
+      // Disable the default base styles:
+      applyBaseStyles: false,
+    }),
+    starlight({
+      title: 'Rezepte',
+      defaultLocale: 'de-de',
+      locales: {
+        'de-de': {
+          label: 'Deutsch',
+          lang: 'de-DE'
+        },
+        en: {
+          label: 'English'
+        }
       },
-      en: {
-        label: 'English'
+      customCss: [
+        './src/tailwind.css',
+      ],
+      sidebar: [{
+        label: 'Rezepte',
+        translations: {
+          'en': 'Recipes',
+        },
+        autogenerate: {
+          directory: 'recipes'
+        }
+      }, {
+        label: 'Anleitungen',
+        translations: {
+          'en': 'Guides',
+        },
+        autogenerate: {
+          directory: 'guides'
+        }
+      }],
+      components: {
+        MarkdownContent: './src/components/recipe.astro'
       }
-    },
-    customCss: [
-      './src/tailwind.css',
-    ],
-    sidebar: [{
-      label: 'Rezepte',
-      translations: {
-        'en': 'Recipes',
-      },
-      autogenerate: {
-        directory: 'recipes'
-      }
-    }, {
-      label: 'Anleitungen',
-      translations: {
-        'en': 'Guides',
-      },
-      autogenerate: {
-        directory: 'guides'
-      }
-    }],
-    components: {
-      MarkdownContent: './src/components/recipe.astro'
-    }
-  }), markdoc()]
+    }),
+    markdoc(),
+  ]
 });
